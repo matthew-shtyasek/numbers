d7b5b25620d1104ba965e68a9f9304ac186c3f3a
Matthew <abcd@mail.u>
2023-01-28 12:20:48 +0500

    __iadd__

--- comment.py
+++ comment.py
@@ -1,15 +1,20 @@
+import copy
+
+
 class Comment:
     def __init__(self, authors, text):
         self.authors = authors
         self.text = text
 
     def __add__(self, other):
-        result = Comment(self.authors, self.text)
-        result.authors += other.authors
-        result.text += other.text
+        result = Comment(copy.copy(self.authors), self.text)
+        result += other
         return result
 
-    
+    def __iadd__(self, other):  # +=
+        self.authors += other.authors
+        self.text += other.text
+        return self
 
     def __str__(self):
         return f'{self.authors}\n{self.text}'
@@ -17,4 +22,14 @@ class Comment:
 
 a = Comment(['Vasya'], "abcdefg")
 b = Comment(['Чукча', "BBC"], "абвгдеёжз")
-print(a+b)
+print(a + b)
+print(a)
+a += b
+print(a)
+
+a1 = [['Vasya'], "abcdefg"]
+b1 = [['Чукча', "BBC"], "абвгдеёжз"]
+
+a1 += b1
+
+print(a1)
